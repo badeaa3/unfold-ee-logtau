f504ab350f841b5a6c4bb5ffb21637ef2fbd472a
Anthony Badea <badea@login14.chn.perlmutter.nersc.gov>
2024-10-09 14:00:57 -0700

    nersc training

--- train_slurm.py
+++ train_slurm.py
@@ -5,7 +5,7 @@ Date: May 27, 2024
 
 # fix for keras v3.0 update
 import os
-os.environ['TF_USE_LEGACY_KERAS'] = '1' 
+# os.environ['TF_USE_LEGACY_KERAS'] = '1' # THIS NEEDS TO BE COMMENTED OUT IF ON NERSC
 
 # python based
 import tensorflow as tf
@@ -19,6 +19,7 @@ import submitit
 import h5py
 import numpy as np
 import shutil
+import subprocess
 
 # custom code
 import dataloader
@@ -26,24 +27,39 @@ import dataloader
 # omnifold
 import omnifold
 
+# SLURM sets CUDA_VISIBLE_DEVICES, so only the allocated GPU is visible to the task
+# gpu_id = os.environ.get('CUDA_VISIBLE_DEVICES')
+# print(f"Assigned GPU: {gpu_id}")
+
 # set gpu growth
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-  try:
-    # Currently, memory growth needs to be the same across GPUs
-    for gpu in gpus:
-      tf.config.experimental.set_memory_growth(gpu, True)
-    logical_gpus = tf.config.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
-  except RuntimeError as e:
-    # Memory growth must be set before GPUs have been initialized
-    print(e)
-
-
+def set_gpu_growth():
+  gpus = tf.config.list_physical_devices('GPU')
+  if gpus:
+    try:
+      # Currently, memory growth needs to be the same across GPUs
+      for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+        try:
+          import horovod.tensorflow as hvd
+          hvd.init()
+          tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
+        except:
+          print("No horovod")
+      logical_gpus = tf.config.list_logical_devices('GPU')
+      print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+      # Memory growth must be set before GPUs have been initialized
+      print(e)
+        
 def train(
     conf
 ):
 
+    # SLURM sets CUDA_VISIBLE_DEVICES, so only the allocated GPU is visible to the task
+    set_gpu_growth()
+    gpu_id = os.environ.get('CUDA_VISIBLE_DEVICES')
+    print(f"Assigned GPU: {gpu_id}")
+    
     print(conf)
     
     # update %j with actual job number
@@ -57,11 +73,11 @@ def train(
     output_directory = Path(str(output_directory).replace("%j", job_id))    
     os.makedirs(output_directory, exist_ok=True)
     print(output_directory)
-
+    
     # write conf to json in output directory for logging
-    output_conf_name = Path(output_directory, "./conf.json").resolve()
-    with open(output_conf_name, 'w') as file:
-      json.dump(conf, file, indent=4)  # indent=4 for pretty printing
+    # output_conf_name = Path(output_directory, "./conf.json").resolve()
+    # with open(output_conf_name, 'w') as file:
+    #   json.dump(conf, file, indent=4)  # indent=4 for pretty printing
     
     # load the aleph reconstructed data, reconstructed mc, and generator mc
     reco_data, reco_mc, gen_mc, pass_reco, pass_gen = dataloader.DataLoader(conf)
@@ -69,18 +85,6 @@ def train(
     # create the event weights
     weights_mc = np.ones(gen_mc.shape[0], dtype=np.float32)
     weights_data = np.ones(reco_data.shape[0], dtype=np.float32)
-
-    # # if user wants to use poisson variation for bootstrapping
-    # if conf["poisson_weights"] != None:
-
-    #   np.random.seed(conf["nstrap"])
-      
-    #   if conf["poisson_weights"] == "mc":
-    #     weights_mc = np.random.poisson(1, gen_mc.shape[0])
-        
-    #   if conf["poisson_weights"] == "data":
-    #     weights_data = np.random.poisson(1, reco_data.shape[0])
-
     
     # make omnifold dataloaders ready for training
     data = omnifold.DataLoader(
@@ -101,14 +105,16 @@ def train(
     )
     
     # make weights directory
-    weights_folder = Path(output_directory, "./model_weights").resolve()
+    # weights_folder = Path(output_directory, "./model_weights").resolve() "%08x" % random.randrange(16**8)
+    weights_folder_id = "%08x" % random.randrange(16**8)
+    weights_folder = Path(output_directory, f"./model_weights_{weights_folder_id}").resolve()
     weights_folder.mkdir()
     weights_folder = str(weights_folder)
 
-    # for itrial in range(conf['ntrial']):
-
-      # clear previous session
-      # K.clear_session()
+    # write conf to json in output directory for logging
+    output_conf_name = Path(weights_folder, "./conf.json").resolve()
+    with open(output_conf_name, 'w') as file:
+      json.dump(conf, file, indent=4)  # indent=4 for pretty printing
       
     # prepare networks
     ndim = reco_data.shape[1] # Number of features we are going to create = thrust
@@ -129,20 +135,18 @@ def train(
       epochs = conf["epochs"],
       lr = conf["lr"],
       niter = conf["niter"],
-      # verbose = conf["verbose"],
+      verbose = conf["verbose"],
       early_stop = conf["early_stop"],
     )
     
     # launch training
-    # mfold.Preprocessing()
     mfold.Unfold()
     
     # get weights
     omnifold_weights = mfold.reweight(gen_mc[pass_gen], mfold.model2)
     
     # save weights to h5 file
-    # outFileName = Path(output_directory, f"omnifold_weights_trial{itrial}.h5").resolve()
-    outFileName = Path(output_directory, f"omnifold_weights.h5").resolve()
+    outFileName = Path(weights_folder, f"omnifold_weights.h5").resolve()
     with h5py.File(outFileName, 'w') as hf:
       hf.create_dataset("weights", data=omnifold_weights)
       
@@ -158,11 +162,6 @@ if __name__ == "__main__":
     parser.add_argument('--run_bootstrap_data', action='store_true', default=False,help='Run the bootstrapping for data')
     parser.add_argument('--run_ensembling', action='store_true', default=False,help='Run the ensembling by retraining without changing the inputs')
     args = parser.parse_args()
-
-    # double check the user didn't give a confusing setting
-    # if args.run_bootstrap_mc == True and args.run_bootstrap_data == True:
-    #   print("Warning you set bootstrap mc and data to True. Exiting to make sure that's what you want")
-    #   exit()
         
     # read in query
     if Path(args.slurm).resolve().exists():
@@ -179,19 +178,16 @@ if __name__ == "__main__":
     # shared training configuration
     training_conf = {
       'output_directory' : str(top_dir),
-      'FILE_MC':'/home/badea/e+e-/aleph/data/processed/20220514/alephMCRecoAfterCutPaths_1994_ThrustReprocess.npz',
-      'FILE_DATA':'/home/badea/e+e-/aleph/data/processed/20220514/LEP1Data1994_recons_aftercut-MERGED_ThrustReprocess.npz',
-      'TrackVariation': 0, # nominal track selection
+      'FILE_MC':'/global/homes/b/badea/aleph/data/processed/20220514/alephMCRecoAfterCutPaths_1994_ThrustReprocess.npz',
+      'FILE_DATA':'/global/homes/b/badea/aleph/data/processed/20220514/LEP1Data1994_recons_aftercut-MERGED_ThrustReprocess.npz',
+      'TrackVariation': 1, # nominal track selection
       'EvtVariation': 0, # nominal event selection
-      #'ntrial': 1, # number of times to run multifold entirely
       'niter': 3,
       'lr': 1e-4,
       'batch_size': 128,
       'epochs': 50,
       'early_stop': 10,
-      #'verbose' : args.verbose,
-      #'poisson_weights' : None,
-      #'strapn' : 0,
+      'verbose' : args.verbose,
       'job_type' : None,
       'i_ensemble_per_omnifold': 0, # i-th ensemble per omnifold
     }
@@ -205,16 +201,16 @@ if __name__ == "__main__":
     18*N trainings for systematic variations
     = 138*N
     '''
-    n_ensemble_per_omnifold = 25
+    n_ensemble_per_omnifold = 3 # this will be scaled by 4 from the running 4 copies on each node
     
     # list of configurations to launch
     confs = []
 
     # add configurations for track and event selection systematic variations
     if args.run_systematics:
-      for TrackVariation in range(0, 9):
-        for EvtVariation in range(0, 2):
-          for iN in range(n_ensemble_per_omnifold):
+      for iN in range(n_ensemble_per_omnifold):
+        for TrackVariation in range(0, 9):
+          for EvtVariation in range(0, 2):
             temp = training_conf.copy()
             temp["TrackVariation"] = TrackVariation
             temp["EvtVariation"] = EvtVariation
@@ -225,10 +221,9 @@ if __name__ == "__main__":
     # bootstrap mc
     n_bootstraps_mc = 40
     if args.run_bootstrap_mc:
-      for strapn in range(n_bootstraps_mc):
-        for iN in range(n_ensemble_per_omnifold):
+      for iN in range(n_ensemble_per_omnifold):
+        for strapn in range(n_bootstraps_mc):
           temp = training_conf.copy()
-          # temp["poisson_weights"] = "mc"
           temp["job_type"] = "BootstrapMC"
           temp["i_ensemble_per_omnifold"] = iN
           confs.append(temp)
@@ -236,10 +231,9 @@ if __name__ == "__main__":
     # bootstrap data
     n_bootstraps_data = 40
     if args.run_bootstrap_data:
-      for strapn in range(n_bootstraps_data):
-        for iN in range(n_ensemble_per_omnifold):
+      for iN in range(n_ensemble_per_omnifold):
+        for strapn in range(n_bootstraps_data):
           temp = training_conf.copy()
-          # temp["poisson_weights"] = "data"
           temp["job_type"] = "BootstrapData"
           temp["i_ensemble_per_omnifold"] = iN
           confs.append(temp)
@@ -247,8 +241,8 @@ if __name__ == "__main__":
     # add configurations for ensembling
     n_ensembles = 40
     if args.run_ensembling:
-      for i in range(n_ensembles):
-        for iN in range(n_ensemble_per_omnifold):
+      for iN in range(n_ensemble_per_omnifold):
+        for i in range(n_ensembles):
           temp = training_conf.copy()
           temp["job_type"] = "Ensembling"
           temp["i_ensemble_per_omnifold"] = iN
