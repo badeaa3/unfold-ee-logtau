2929b2d88ce39f870b316b4b9d0bdcd0e84aa176
Anthony Badea <badea@login12.chn.perlmutter.nersc.gov>
2025-03-07 15:46:53 -0800

    working

--- dataloader.py
+++ dataloader.py
@@ -9,42 +9,41 @@ def DataLoader(config):
 
     # pick up the correct entries
     i = config["SystematicVariation"]
+    # data systematic variation
     data = np.array([x[i] for x in np.array(f_data["t/Thrust"])]) # data
     data_mask = np.array([x[i] for x in np.array(f_data["t/passEventSelection"])])
+    # mc reco systematic variation
     mc_reco = np.array([x[i] for x in np.array(f_mc["t/Thrust"])]) # reco level
-    reco_mask = np.array([x[i] for x in np.array(f_mc["t/passEventSelection"])])
-    mc_gen = np.array(f_mc["tgenBefore/Thrust"]) # generator level without hadronic event selection
-    gen_mask = np.array(f_mc["tgenBefore/passEventSelection"])
+    mc_reco_mask = np.array([x[i] for x in np.array(f_mc["t/passEventSelection"])])
+    # generator level only one element
+    mc_gen = np.array([x[0] for x in np.array(f_mc["tgenBefore/Thrust"])]) # generator level without hadronic event selection
+    mc_gen_mask = np.array([x[0] for x in np.array(f_mc["tgenBefore/passEventSelection"])])
 
     ###### pick up the correct event ID's to use directly tgenbefore
     a = np.array(f_mc["tgen/uniqueID"]) # generator level with hadronic event selection, events matched to reco
     b = np.array(f_mc["tgenBefore/uniqueID"]) # generator level without hadronic event selection
     intersect, ind_a, ind_b = np.intersect1d(a, b, return_indices=True)
 
+    # make temporary reco lists with same size as tgenBefore
     pass_reco = np.zeros(len(b))
-    pass_reco[ind_b] = reco_mask[ind_a]
-
     reco_vals = -999.*np.ones(len(b))
+    # now populate the list with the reco values according to the uniqueID order of tgenBefore
+    pass_reco[ind_b] = mc_reco_mask[ind_a]
     reco_vals[ind_b] = mc_reco[ind_a]
-
+    # now update reco values
     mc_reco = reco_vals
-    reco_mask = pass_reco
+    mc_reco_mask = pass_reco
 
-    # append to reco to make the same length as tgenbefore
-    diff = mc_gen.shape[0] - mc_reco.shape[0]
-    mc_reco = np.concatenate([mc_reco, np.ones(diff)])
-    reco_mask = np.concatenate([reco_mask, np.ones(diff).astype(bool)])
+    # final preparation
+    data = np.expand_dims(data[data_mask],-1) # We only want data events passing a selection criteria
+    mc_reco = np.expand_dims(mc_reco, -1) # expand dimension
+    mc_reco_mask = mc_reco_mask == 1 # convert to boolean mask
+    mc_gen = np.expand_dims(mc_gen, -1) # expand dimension
+    mc_gen_mask = mc_gen_mask == 1 # convert to boolean mask
     
-    # We only want data events passing a selection criteria
-    data = np.expand_dims(data[data_mask],-1)
-    mc_reco = np.expand_dims(mc_reco, -1)
-    reco_mask = reco_mask == 1
-    mc_gen = np.expand_dims(mc_gen,-1)
-    gen_mask = gen_mask == 1
+    return data, mc_reco, mc_gen, mc_reco_mask, mc_gen_mask
     
-    return data, mc_reco, mc_gen, reco_mask, gen_mask
-    
-# def DataLoader(config):
+# def DataLoaderOld(config):
 
 #     # load everything
 #     DATA = np.load(config['FILE_DATA'], allow_pickle=True)
@@ -54,50 +53,61 @@ def DataLoader(config):
 #     data = DATA["t_thrust"][:,config['TrackVariation']]
 #     data_mask = DATA[f"t_passEventSelection_{config['EvtVariation']}"][:,config['TrackVariation']]
 #     mc_reco = MC["t_thrust"][:,config['TrackVariation']]
-#     reco_mask = MC[f"t_passEventSelection_{config['EvtVariation']}"][:,config['TrackVariation']]
+#     mc_reco_mask = MC[f"t_passEventSelection_{config['EvtVariation']}"][:,config['TrackVariation']]
 #     mc_gen = np.stack(MC["tgenBefore_thrust"]).flatten() # little hack because format is array([array([]), array([]), ....]) and we need array([,,,,])
-#     gen_mask = np.stack(MC[f"tgenBefore_passEventSelection"]).flatten()
-    
+#     mc_gen_mask = np.stack(MC[f"tgenBefore_passEventSelection"]).flatten()
+#     print(mc_reco.shape, mc_gen.shape)
+
 #     ###### pick up the correct event ID's to use directly tgenbefore
 #     a = MC['tgen_uniqueID'] # WHES_id
 #     b = MC['tgenBefore_uniqueID'] # WHOES_id
 #     intersect, ind_a, ind_b = np.intersect1d(a, b, return_indices=True)
-    
+#     print(ind_a.shape, ind_b.shape,  a.shape, b.shape)
+
 #     pass_reco = np.zeros(len(b))
-#     pass_reco[ind_b] = reco_mask[ind_a]
+#     pass_reco[ind_b] = mc_reco_mask[ind_a]
     
 #     reco_vals = -999.*np.ones(len(b))
 #     reco_vals[ind_b] = mc_reco[ind_a]
 
 #     mc_reco = reco_vals
-#     reco_mask = pass_reco
+#     mc_reco_mask = pass_reco
+#     # print(mc_reco.shape, mc_gen.shape)
 #     ########
     
 #     # append to reco to make the same length as tgenbefore
 #     diff = mc_gen.shape[0] - mc_reco.shape[0]
+#     # print(diff)
 #     mc_reco = np.concatenate([mc_reco, np.ones(diff)])
-#     reco_mask = np.concatenate([reco_mask, np.ones(diff).astype(bool)])
-#     print(data.shape, data_mask.shape, mc_reco.shape, reco_mask.shape, mc_gen.shape, gen_mask.shape)
+#     mc_reco_mask = np.concatenate([mc_reco_mask, np.ones(diff).astype(bool)])
     
 #     #We only want data events passing a selection criteria
 #     data = np.expand_dims(data[data_mask],-1)
 #     mc_reco = np.expand_dims(mc_reco, -1)
-#     reco_mask = reco_mask == 1
+#     mc_reco_mask = mc_reco_mask == 1
 #     mc_gen = np.expand_dims(mc_gen,-1)
-#     gen_mask = gen_mask == 1
+#     mc_gen_mask = mc_gen_mask == 1
     
-#     return data, mc_reco, mc_gen, reco_mask, gen_mask
+#     return data, mc_reco, mc_gen, mc_reco_mask, mc_gen_mask
 
 if __name__ == "__main__":
 
-    # data, mc_reco, mc_gen, reco_mask, gen_mask = DataLoader(yaml.safe_load(open("config_omnifold_test.json")))
-    # print(data.shape, mc_reco.shape, reco_mask.shape, mc_gen.shape, gen_mask.shape)
+    # training_conf = {
+    #   'FILE_MC':'/global/homes/b/badea/aleph/data/processed/20220514/alephMCRecoAfterCutPaths_1994_ThrustReprocess.npz',
+    #   'FILE_DATA':'/global/homes/b/badea/aleph/data/processed/20220514/LEP1Data1994_recons_aftercut-MERGED_ThrustReprocess.npz',
+    #   'TrackVariation': 1, # nominal track selection as written here https://github.com/badeaa3/ALEPHOmnifold/blob/main/src/Thrust.cxx#L143
+    #   'EvtVariation': 0, # nominal event selection
+    # }
+    # data, mc_reco, mc_gen, mc_reco_mask, mc_gen_mask = DataLoaderOld(training_conf)
+    # print(data.shape, mc_reco.shape, mc_reco_mask.shape, mc_gen.shape, mc_gen_mask.shape)
+    # print(data[:5], mc_reco[:5], mc_gen[:5], mc_reco_mask[:5], mc_gen_mask[:5])
+    # print("new")
 
     training_conf = {
-      'f_mc':'/eos/home-a/abadea/data/aleph/unfold-ee-logtau/DataProcessing/temp/5/alephMCRecoAfterCutPaths_1994_thrust.root',
-      'f_data':'/eos/home-a/abadea/data/aleph/unfold-ee-logtau/DataProcessing/LEP1Data1994_recons_aftercut-MERGED_thrust.root',
+      'f_data':'/global/homes/b/badea/aleph/data/ThrustDerivation/030725/LEP1Data1994_recons_aftercut-MERGED_thrust.root',
+      'f_mc':'/global/homes/b/badea/aleph/data/ThrustDerivation/030725/alephMCRecoAfterCutPaths_1994_thrust.root',
       'SystematicVariation': 1, # chosen selection
     }
-    data, mc_reco, mc_gen, reco_mask, gen_mask = DataLoader(training_conf)
-    print(data.shape, mc_reco.shape, reco_mask.shape, mc_gen.shape, gen_mask.shape)
-    print(data[:5], mc_reco[:5], mc_gen[:5], reco_mask[:5], gen_mask[:5])
+    data, mc_reco, mc_gen, mc_reco_mask, mc_gen_mask = DataLoader(training_conf)
+    print(data.shape, mc_reco.shape, mc_reco_mask.shape, mc_gen.shape, mc_gen_mask.shape)
+    print(data[:5], mc_reco[:5], mc_gen[:5], mc_reco_mask[:5], mc_gen_mask[:5])
--- train_slurm.py
+++ train_slurm.py
@@ -11,7 +11,7 @@ import os
 import tensorflow as tf
 import tensorflow.keras.backend as K
 import random
-from pathlib import Path
+# from pathlib import Path
 import time
 import argparse
 import json
@@ -70,7 +70,7 @@ def train(
     except:
         job_id = "%08x" % random.randrange(16**8)
         
-    output_directory = Path(str(output_directory).replace("%j", job_id))    
+    output_directory = os.path.abspath(output_directory.replace("%j", job_id))
     os.makedirs(output_directory, exist_ok=True)
     print(output_directory)
         
@@ -102,18 +102,21 @@ def train(
     # make weights directory
     # weights_folder = Path(output_directory, "./model_weights").resolve() "%08x" % random.randrange(16**8)
     weights_folder_id = "%08x" % random.randrange(16**8)
-    weights_folder = Path(output_directory, f"./model_weights_{weights_folder_id}").resolve()
-    weights_folder.mkdir()
-    weights_folder = str(weights_folder)
+    weights_folder = os.path.abspath(os.path.join(output_directory, f"./model_weights_{weights_folder_id}"))
+    os.makedirs(weights_folder, exist_ok=True)
+    # weights_folder.mkdir()
+    # weights_folder = str(weights_folder)
 
     # save the starting weights
-    outFileName = Path(weights_folder, f"starting_weights.h5").resolve()
-    with h5py.File(outFileName, 'w') as hf:
-      hf.create_dataset("weights_data", data=data.weight)
-      hf.create_dataset("weights_mc", data=mc.weight)
+    outFileName = os.path.abspath(os.path.join(weights_folder, "starting_weights.npz"))
+    np.savez(outFileName, weights_data=data.weight, weights_mc=mc.weight)
+    # outFileName = os.path.abspath(os.path.join(weights_folder, "starting_weights.h5"))
+    # with h5py.File(outFileName, 'w') as hf:
+    #   hf.create_dataset("weights_data", data=data.weight)
+    #   hf.create_dataset("weights_mc", data=mc.weight)
       
     # write conf to json in output directory for logging
-    output_conf_name = Path(weights_folder, "./conf.json").resolve()
+    output_conf_name = os.path.abspath(os.path.join(weights_folder, "conf.json"))
     with open(output_conf_name, 'w') as file:
       json.dump(conf, file, indent=4)  # indent=4 for pretty printing
     
@@ -146,9 +149,11 @@ def train(
     omnifold_weights = mfold.reweight(gen_mc[pass_gen], mfold.model2, batch_size=1000)
     
     # save weights to h5 file
-    outFileName = Path(weights_folder, f"omnifold_weights.h5").resolve()
-    with h5py.File(outFileName, 'w') as hf:
-      hf.create_dataset("weights", data=omnifold_weights)
+    # outFileName = os.path.abspath(os.path.join(weights_folder, "omnifold_weights.h5"))
+    # with h5py.File(outFileName, 'w') as hf:
+    #   hf.create_dataset("weights", data=omnifold_weights)
+    outFileName = os.path.abspath(os.path.join(weights_folder, "omnifold_weights.npy"))
+    np.save(outFileName, omnifold_weights)
       
 if __name__ == "__main__":
 
@@ -156,44 +161,46 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--slurm", help="path to json file containing slurm configuration", default=None)
     parser.add_argument("--njobs", help="number of jobs to actually launch. default is all", default=-1, type=int)
-    parser.add_argument('--verbose', action='store_true', default=False,help='Run the scripts with more verbose output')
-    parser.add_argument('--run_systematics', action='store_true', default=False,help='Run the track and event selection systematic variations')
-    parser.add_argument('--run_bootstrap_mc', action='store_true', default=False,help='Run the bootstrapping for MC')
-    parser.add_argument('--run_bootstrap_data', action='store_true', default=False,help='Run the bootstrapping for data')
-    parser.add_argument('--run_ensembling', action='store_true', default=False,help='Run the ensembling by retraining without changing the inputs')
+    parser.add_argument('--verbose', action='store_true', default=False, help='Run the scripts with more verbose output')
+    parser.add_argument('--run_systematics', action='store_true', default=False, help='Run the track and event selection systematic variations')
+    parser.add_argument('--run_bootstrap_mc', action='store_true', default=False, help='Run the bootstrapping for MC')
+    parser.add_argument('--run_bootstrap_data', action='store_true', default=False, help='Run the bootstrapping for data')
+    parser.add_argument('--run_ensembling', action='store_true', default=False, help='Run the ensembling by retraining without changing the inputs')
     args = parser.parse_args()
         
     # read in query
-    if Path(args.slurm).resolve().exists():
-        query_path = Path(args.slurm).resolve()
-    else:
-        # throw
-        raise ValueError(f"Could not locate {args.slurm} in query directory or as absolute path")
+    query_path = os.path.abspath(args.slurm)
+    if not os.path.exists(query_path):
+      raise ValueError(f"Could not locate {args.slurm}")
     with open(query_path) as f:
-        query = json.load(f)
+      query = json.load(f)
 
     # create top level output directory
-    top_dir = Path("results", f'./training-{"%08x" % random.randrange(16**8)}', "%j").resolve()
+    top_dir = os.path.abspath(os.path.join("results", f'training-{"%08x" % random.randrange(16**8)}', "%j"))
 
     # shared training configuration
-    training_conf = {
-      'output_directory' : str(top_dir),
-      # 'FILE_MC':'/global/homes/b/badea/aleph/data/processed/20220514/alephMCRecoAfterCutPaths_1994_ThrustReprocess.npz',
-      # 'FILE_DATA':'/global/homes/b/badea/aleph/data/processed/20220514/LEP1Data1994_recons_aftercut-MERGED_ThrustReprocess.npz',
-      # 'TrackVariation': 1, # nominal track selection as written here https://github.com/badeaa3/ALEPHOmnifold/blob/main/src/Thrust.cxx#L143
-      # 'EvtVariation': 0, # nominal event selection
-      'f_mc':'/eos/home-a/abadea/data/aleph/unfold-ee-logtau/DataProcessing/temp/5/alephMCRecoAfterCutPaths_1994_thrust.root',
-      'f_data':'/eos/home-a/abadea/data/aleph/unfold-ee-logtau/DataProcessing/LEP1Data1994_recons_aftercut-MERGED_thrust.root',
-      'SelectionIDX': 1, # chosen selection
-      'niter': 3,
-      'lr': 1e-4,
-      'batch_size': 128,
-      'epochs': 50,
-      'early_stop': 10,
-      'verbose' : args.verbose,
-      'job_type' : None,
-      'i_ensemble_per_omnifold': 0, # i-th ensemble per omnifold
-    }
+    # training_conf = {
+    #   'output_directory' : top_dir,
+    #   'f_mc':'/eos/home-a/abadea/data/aleph/unfold-ee-logtau/DataProcessing/alephMCRecoAfterCutPaths_1994_thrust.root',
+    #   'f_data':'/eos/home-a/abadea/data/aleph/unfold-ee-logtau/DataProcessing/LEP1Data1994_recons_aftercut-MERGED_thrust.root',
+    #   'SystematicVariation': 1, # nominal=1
+    #   'niter': 3,
+    #   'lr': 1e-4,
+    #   'batch_size': 128,
+    #   'epochs': 50,
+    #   'early_stop': 10,
+    #   'verbose' : args.verbose,
+    #   'job_type' : None,
+    #   'i_ensemble_per_omnifold': 0, # i-th ensemble per omnifold
+    # }
+    with open("training_conf.json") as f:
+      training_conf = json.load(f)
+    training_conf["output_directory"] = top_dir
+    training_conf["verbose"] = args.verbose
+    # training_conf["epochs"] = 1
+    # training_conf["niter"] = 2
+    print(training_conf)
+    # exit()
 
     # number of repeated trainings per omnifold configuration
     '''
@@ -215,51 +222,36 @@ if __name__ == "__main__":
     total_n_systematics = 12 # closest to 10 which divides by 4
     n_systematics = int(total_n_systematics / n_training_per_node)
     if args.run_systematics:
-      for iN in range(n_systematics):
+      for i in range(n_systematics):
 
         # sysematic variation
         # 1 = nominal, 2 onwards = variations
-        for SystematicVariation in range(1, 13):
+        SystematicVariationList = [2, 3, 4, 5, 6] # according to the order in DataProcessing/Thrust.cxx
+        for SystematicVariation in SystematicVariationList:
           temp = training_conf.copy()
           temp["SystematicVariation"] = SystematicVariation
           temp["job_type"] = "Systematics"
-          temp["i_ensemble_per_omnifold"] = iN
+          temp["i_ensemble_per_omnifold"] = i
           confs.append(temp)
-
-        # # track selections defined https://github.com/badeaa3/ALEPHOmnifold/blob/main/src/Thrust.cxx#L141-L150
-        # for TrackVariation in range(2, 9):
-        #   temp = training_conf.copy()
-        #   temp["TrackVariation"] = TrackVariation
-        #   temp["job_type"] = "Systematics"
-        #   temp["i_ensemble_per_omnifold"] = iN
-        #   confs.append(temp)
-
-        # # event selection defined https://github.com/badeaa3/ALEPHOmnifold/blob/main/src/Thrust.cxx#L192-L197
-        # for EvtVariation in [1]:
-        #   temp = training_conf.copy()
-        #   temp["EvtVariation"] = EvtVariation
-        #   temp["job_type"] = "Systematics"
-        #   temp["i_ensemble_per_omnifold"] = iN
-        #   confs.append(temp)
 
     # bootstrap mc
     total_n_bootstraps_mc = 40
     n_bootstraps_mc = int(total_n_bootstraps_mc / n_training_per_node)
     if args.run_bootstrap_mc:
-      for strapn in range(n_bootstraps_mc):
+      for i in range(n_bootstraps_mc):
         temp = training_conf.copy()
         temp["job_type"] = "BootstrapMC"
-        temp["i_ensemble_per_omnifold"] = strapn
+        temp["i_ensemble_per_omnifold"] = i
         confs.append(temp)
 
     # bootstrap data
     total_n_bootstraps_data = 40
     n_bootstraps_data = int(total_n_bootstraps_data / n_training_per_node)
     if args.run_bootstrap_data:
-      for strapn in range(n_bootstraps_data):
+      for i in range(n_bootstraps_data):
         temp = training_conf.copy()
         temp["job_type"] = "BootstrapData"
-        temp["i_ensemble_per_omnifold"] = strapn
+        temp["i_ensemble_per_omnifold"] = i
         confs.append(temp)
     
     # add configurations for ensembling
@@ -269,7 +261,7 @@ if __name__ == "__main__":
       for i in range(n_ensembles):
         temp = training_conf.copy()
         temp["job_type"] = "Ensembling"
-        temp["i_ensemble_per_omnifold"] = iN
+        temp["i_ensemble_per_omnifold"] = i
         confs.append(temp)
         
     # if submitit false then just launch job
