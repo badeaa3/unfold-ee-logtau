e519e12ccd4362818b08933d128d3dcfc697cc38
Anthony Badea <badea@x1403c1s4b0n1h0.chn.perlmutter.nersc.gov>
2025-04-03 13:39:36 -0700

    working

--- ReweightMC/ReweightMC.py
+++ ReweightMC/ReweightMC.py
@@ -1,85 +1,20 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.callbacks import EarlyStopping,ModelCheckpoint, ReduceLROnPlateau
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 import numpy as np
-import uproot
+# import uproot
 import pickle
 import os
-import awkward as ak
+# import awkward as ak
 import random
 
 import omnifold
 from ReweightMCDataLoading import *
 
-# training settings
-test_size = 0.2
-lr = 5e-4
-epochs = 10
-batch_size = 512
-verbose = True
-top_dir = "/pscratch/sd/b/badea/aleph/unfold-ee-logtau/ReweightMC/results/"
-weights_folder = os.path.join(top_dir, f'training-{"%08x" % random.randrange(16**8)}') # "./"
-os.makedirs(weights_folder, exist_ok=True)
-new_mc_name = "PYTHIA8"
-model_name = os.path.join(weights_folder, f'Reweight_{new_mc_name}.weights.h5')
-print(model_name)
-branches = ["px", "py", "pz", "mass"] # note: "charge" only exists for files processed with scan.cc (aleph mc, herwig, sherpa) not for the current pythia8 files
-
-if __name__ == "__main__":
+# fitting function
+def doFitAndEvaluate(model, train_dataset, val_dataset, epochs, verbose, callbacks, name, evaluate_data):
     
-    # Particle level distribution reweighting 
-    aleph_mc = loadDataParticles(
-        filePath = mc_paths["ArchivedPYTHIA6"]["path"],
-        treeName = mc_paths["ArchivedPYTHIA6"]["tree"],
-        branches = branches,
-        maxNPart = 80
-    )
-    print(aleph_mc.shape)
-    new_mc = loadDataParticles(
-        filePath = mc_paths[new_mc_name]["path"], 
-        treeName = mc_paths[new_mc_name]["tree"],
-        branches = branches,
-        maxNPart = 80
-    )
-    print(new_mc.shape)
-
-    # create labels
-    labels_aleph_mc = np.ones(len(aleph_mc),dtype=np.float32)
-    labels_new_mc = np.zeros(len(new_mc),dtype=np.float32)
-
-    # concatenate
-    data = np.concatenate((aleph_mc, new_mc))
-    # labels expected to be stack of [labels, weights]
-    labels = np.concatenate((labels_aleph_mc, labels_new_mc))
-    weights = np.ones(len(labels)) # unit weights for all for now
-    labels = np.stack([labels, weights], axis=1).astype(np.float32)
-    print(data.shape, labels.shape)
-
-    # Split into training (80%) and validation (20%) sets randomly
-    train_data, val_data, train_labels, val_labels = train_test_split(data, labels, test_size=test_size, stratify=labels)
-    print(train_data.shape, val_data.shape, train_labels.shape, val_labels.shape)
-
-    # Create TensorFlow datasets
-    train_dataset = tf.data.Dataset.from_tensor_slices((train_data, train_labels))
-    val_dataset = tf.data.Dataset.from_tensor_slices((val_data, val_labels))
-
-    # Batch and shuffle training data
-    train_dataset = train_dataset.shuffle(buffer_size=len(train_data)).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-    val_dataset = val_dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
-
-    # prepare PET network
-    model = omnifold.PET(num_feat = train_data.shape[2], num_evt = 0, num_part = train_data.shape[1], num_heads = 1, num_transformer = 1, local = False, projection_dim = 64)
-
-    # training settings
-    opt = tf.keras.optimizers.Adam(learning_rate=lr)
-    model.compile(opt, loss = omnifold.net.weighted_binary_crossentropy)
-    callbacks = [
-        ReduceLROnPlateau(patience=1000, min_lr=1e-7, verbose=verbose, monitor="val_loss"),
-        EarlyStopping(patience=10, restore_best_weights=True,  monitor="val_loss"),
-        ModelCheckpoint(model_name, save_best_only=True, mode='auto', save_weights_only=True),
-    ]
-
     hist =  model.fit(
         train_dataset,
         epochs = epochs,
@@ -90,9 +25,76 @@ if __name__ == "__main__":
 
     print(f"Last val loss {hist.history['val_loss'][0]}")
     print("INFO: Dumping training history ...")
-    with open(model_name.replace(".weights.h5",".pkl"),"wb") as f:
+    with open(name.replace(".weights.h5",".pkl"),"wb") as f:
         pickle.dump(hist.history, f)
 
-    weights = reweight(aleph_mc, model, batch_size=10000)
+    weights = reweight(evaluate_data, model, batch_size=10000)
     print(weights.shape)
-    np.save(model_name.replace(".weights.h5",".reweight.npy"), weights)
+    np.save(name.replace(".weights.h5",".reweight.npy"), weights)    
+
+    return hist
+
+if __name__ == "__main__":
+    
+    # training settings
+    test_size = 0.2
+    lr = 5e-4
+    epochs = 2
+    batch_size = 512
+    verbose = True
+    top_dir = "/pscratch/sd/b/badea/aleph/unfold-ee-logtau/ReweightMC/results/"
+    weights_folder = os.path.join(top_dir, f'training-{"%08x" % random.randrange(16**8)}') # "./"
+    os.makedirs(weights_folder, exist_ok=True)
+    new_mc_name = "PYTHIA8"
+    maxNPart = 80
+
+    # Particle level distribution reweighting 
+    aleph_mc = loadDataParticles(
+        filePath = mc_paths["ArchivedPYTHIA6"]["path"],
+        treeName = mc_paths["ArchivedPYTHIA6"]["tree"],
+        branches = mc_paths["ArchivedPYTHIA6"]["branches"],
+        maxNPart = maxNPart
+    )
+    print(aleph_mc.shape)
+    new_mc = loadDataParticles(
+        filePath = mc_paths[new_mc_name]["path"], 
+        treeName = mc_paths[new_mc_name]["tree"],
+        branches = mc_paths[new_mc_name]["branches"],
+        maxNPart = maxNPart
+    )
+    print(new_mc.shape)
+
+    # prepare datasets
+    train_dataset_step1, val_dataset_step1 = create_train_val_datasets(data_0=aleph_mc, data_1=aleph_mc, test_size=test_size, batch_size=batch_size, normalize=True)
+    train_dataset_step2, val_dataset_step2 = create_train_val_datasets(data_0=new_mc, data_1=aleph_mc, test_size=test_size, batch_size=batch_size, normalize=True)
+
+    # prepare PET network
+    model = omnifold.PET(
+        num_feat = aleph_mc.shape[2], 
+        num_evt = 0, 
+        num_part = maxNPart, 
+        num_heads = 1, 
+        num_transformer = 1, 
+        local = False,
+        projection_dim = 64
+    )
+
+    # training settings
+    opt = tf.keras.optimizers.Adam(learning_rate=lr)
+    model.compile(opt, loss = omnifold.net.weighted_binary_crossentropy)
+    callbacks = [
+        ReduceLROnPlateau(patience=1000, min_lr=1e-7, verbose=verbose, monitor="val_loss"),
+        EarlyStopping(patience=10, restore_best_weights=True,  monitor="val_loss"),
+    ]
+
+    # Step 1 pretrain the model to get it to unity
+    model_name = os.path.join(weights_folder, f'Reweight_Step1_ALEPHMC.weights.h5')
+    callbacks.append(ModelCheckpoint(model_name, save_best_only=True, mode='auto', save_weights_only=True))
+    print("Running Step 1 with model name: ", model_name)
+    hist = doFitAndEvaluate(model, train_dataset_step1, val_dataset_step1, epochs, verbose, callbacks, model_name, aleph_mc) 
+
+    # Step 2 train the model for reweighting
+    model_name = os.path.join(weights_folder, f'Reweight_Step2_{new_mc_name}.weights.h5')
+    callbacks.append(ModelCheckpoint(model_name, save_best_only=True, mode='auto', save_weights_only=True))
+    print("Running Step 2 with model name: ", model_name)
+    hist = doFitAndEvaluate(model, train_dataset_step2, val_dataset_step2, epochs, verbose, callbacks, model_name, aleph_mc)
--- ReweightMC/ReweightMCDataLoading.py
+++ ReweightMC/ReweightMCDataLoading.py
@@ -1,32 +1,40 @@
 import uproot
 import numpy as np
 import awkward as ak
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
 
 # file paths
 mc_paths = {
     "ArchivedPYTHIA6" : {
         "path" : "/pscratch/sd/b/badea/aleph/data/alephMCRecoAfterCutPaths_1994.root",
-        "tree" : "tgenBefore" # t=reco, tgen = generator level after hadronic event selection, tgenBefore = generator level before hadronic event selection
+        "tree" : "tgenBefore", # t=reco, tgen = generator level after hadronic event selection, tgenBefore = generator level before hadronic event selection
+        "branches" : ["pmag", "eta", "phi"],
     },
     "HERWIG7" : {
         "path" : "/pscratch/sd/b/badea/aleph/data/LEP1MCVariations/abaty/HERWIG7/2_10_2024_LEP1MC/LEP-Matchbox-S1000-1_0_0.root",
-        "tree" : "t"
+        "tree" : "t",
+        "branches" : ["pmag", "eta", "phi"],
     },
     "SHERPA" : {
         "path" : "/pscratch/sd/b/badea/aleph/data/LEP1MCVariations/abaty/SHERPA/2_10_2024_LEP1MC/Sherpa_RNG100_0_0.root",
-        "tree" : "t"
+        "tree" : "t",
+        "branches" : ["pmag", "eta", "phi"],
     },
     "PYTHIA8" : {
         "path" : "/pscratch/sd/b/badea/aleph/data/LEP1MCVariations/hannah/LEP1_pythia8_MC_withSphericity_v2.root",
-        "tree" : "tgen"
+        "tree" : "tgen",
+        "branches" : ["p", "eta", "phi"],
     },
     "PYTHIA8_DIRE" : {
         "path": "/pscratch/sd/b/badea/aleph/data/LEP1MCVariations/hannah/LEP1_pythia8_MC_DIRE.root",
-        "tree" : "tgen"
+        "tree" : "tgen",
+        "branches" : ["p", "eta", "phi"],
     },
     "PYTHIA8_VINCIA" : {
         "path" : "/pscratch/sd/b/badea/aleph/data/LEP1MCVariations/hannah/LEP1_pythia8_MC_VINCIA.root",
-        "tree" : "tgen"
+        "tree" : "tgen",
+        "branches" : ["p", "eta", "phi"],
     }
 }
 
@@ -61,3 +69,43 @@ def loadDataParticles(filePath, treeName, branches, maxNPart, padValue = 0):
     # stack and return
     data = np.stack(data, axis=-1)
     return data
+
+def create_train_val_datasets(data_0, data_1, test_size=0.2, batch_size=512, normalize=True):
+    
+    # create labels
+    labels_data_0 = np.zeros(len(data_0),dtype=np.float32)
+    labels_data_1 = np.ones(len(data_1),dtype=np.float32)
+
+    # create weights
+    weights_data_0 = np.ones(len(data_0),dtype=np.float32)
+    weights_data_1 = np.ones(len(data_1),dtype=np.float32)  
+
+    # normalize
+    if normalize:
+        weights_data_1 *= (np.sum(weights_data_0) / np.sum(weights_data_1))
+        print(weights_data_0.sum(), weights_data_1.sum())
+
+    # concatenate
+    data = np.concatenate((data_0, data_1))
+    # labels expected to be stack of [labels, weights]
+    labels = np.concatenate((labels_data_0, labels_data_1))
+    weights = np.concatenate((weights_data_0, weights_data_1))
+    labels = np.stack([labels, weights], axis=1).astype(np.float32)
+    # print(data.shape, labels.shape)
+
+    # standardize data
+    # data = (data - np.mean(data, axis=0)) / np.std(data, axis=0)
+
+    # Split into training (80%) and validation (20%) sets randomly
+    train_data, val_data, train_labels, val_labels = train_test_split(data, labels, test_size=test_size, stratify=labels)
+    # print(train_data.shape, val_data.shape, train_labels.shape, val_labels.shape)
+    
+    # Create TensorFlow datasets
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_data, train_labels))
+    val_dataset = tf.data.Dataset.from_tensor_slices((val_data, val_labels))
+
+    # Batch and shuffle training data
+    train_dataset = train_dataset.shuffle(buffer_size=len(train_data)).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    val_dataset = val_dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+
+    return train_dataset, val_dataset
