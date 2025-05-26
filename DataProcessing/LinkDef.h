465da55691077ffa1faea5496feb14e728a3d776
Anthony Badea <anthony.badea@cern.ch>
2025-05-26 13:39:32 +0200

    linkdef.h

---
+++ DataProcessing/LinkDef.h
@@ -0,0 +1,8 @@
+#include <vector>
+
+#ifdef __ROOTCLING__
+#pragma link C++ class std::vector<float>+;
+#pragma link C++ class std::vector<std::vector<float>>+;
+#pragma link C++ class std::vector<Short_t>+;
+#pragma link C++ class std::vector<std::vector<Short_t>>+;
+#endif
